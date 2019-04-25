feb89352b2b7807595c653073394961ee75f1fdb
Justin Dennison <justin1dennison@gmail.com>
2019-04-25 10:37:50 -0400

    finish beer song

---
+++ beer-song/src/beer_song.clj
@@ -0,0 +1,33 @@
+(ns beer-song
+  (:require [clojure.string :as string]))
+
+
+(defn beer-wall [n]
+  (if (= n 1)
+      (str "1 bottle of beer on the wall")
+      (str n " bottles of beer on the wall")))
+
+(defn verse
+  "Returns the nth verse of the song."
+  [num]
+  (condp = num
+    0 (str "No more bottles of beer on the wall, no more bottles of beer.\n"
+           "Go to the store and buy some more, 99 bottles of beer on the wall.\n")
+    1 (str "1 bottle of beer on the wall, 1 bottle of beer.\n"
+           "Take it down and pass it around, "
+           "no more bottles of beer on the wall.\n")
+    (str (beer-wall num) ", " num " bottles of beer.\n"
+         "Take one down and pass it around, " (beer-wall (dec num)) ".\n")))
+
+
+(defn sing
+  "Given a start and an optional end, returns all verses in this interval. If
+  end is not given, the whole song from start is sung."
+  ([start] (string/join "\n"
+             (into [] 
+                   (map verse 
+                        (range start -1 -1)))))
+  ([start end] (string/join  "\n"
+                 (into [] 
+                       (map verse 
+                            (range start (dec end) -1))))))
