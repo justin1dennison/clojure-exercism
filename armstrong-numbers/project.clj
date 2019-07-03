0e1d974448c80923d0cb70b4f2da640e7399ae9a
Justin Dennison <justin1dennison@gmail.com>
2019-07-03 07:17:08 -0400

    redid the armstrong numbers to not use strings

--- armstrong-numbers/project.clj
+++ armstrong-numbers/project.clj
@@ -1,4 +1,5 @@
 (defproject armstrong-numbers "0.1.0-SNAPSHOT"
   :description "armstrong-numbers exercise"
   :url "https://github.com/exercism/clojure/tree/master/exercises/armstrong-numbers"
+  :plugins [[lein-cljfmt "0.6.4"]]
   :dependencies [[org.clojure/clojure "1.10.0"]])
--- armstrong-numbers/src/armstrong_numbers.clj
+++ armstrong-numbers/src/armstrong_numbers.clj
@@ -3,18 +3,19 @@
 (defn pow [x n]
   (reduce * (repeat n x)))
 
-(defn parse-int [c]
-  (Integer/parseInt (str c)))
-
 (defn sum-of-powers [xs power]
   (reduce + (map #(pow % power) xs)))
 
+(defn number-digits [k]
+  (loop [n k result []]
+    (if (<= n 0)
+      (reverse result)
+      (recur (quot n 10) (conj result (mod n 10))))))
 
-(defn armstrong? [num]
-    (let [s (str num)
-         length (count s)
-         digits (map parse-int s)
-         powsum (sum-of-powers digits length)]
-    (= powsum num)))
+(defn armstrong? [n]
+  (let [digits (number-digits n)
+        length (count digits)
+        powsum (sum-of-powers digits length)]
+    (= powsum n)))
 
 
