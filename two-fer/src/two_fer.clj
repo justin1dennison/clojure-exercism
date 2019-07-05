5c27f788fbda6e86bf139cdfcdfa8ce5114c8da3
Justin Dennison <justin1dennison@gmail.com>
2019-07-04 20:41:25 -0400

    simplified two-fer using multi-arity functions

--- two-fer/src/two_fer.clj
+++ two-fer/src/two_fer.clj
@@ -1,10 +1,5 @@
 (ns two-fer)
 
-
-(defn get-name [args]
-  (if (nil? args)
-    "you"
-    (first args)))
-
-(defn two-fer [& args] 
-  (str "One for " (get-name args) ", one for me."))
+(defn two-fer 
+  ([] "One for you, one for me.")
+  ([name] (str "One for " name ", one for me.")))
