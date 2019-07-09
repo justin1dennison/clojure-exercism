d30be29464ef000be3cf17bf4be32bb0eb7f4411
Justin Dennison <justin1dennison@gmail.com>
2019-07-09 09:26:50 -0400

    combined cases

--- protein-translation/src/protein_translation.clj
+++ protein-translation/src/protein_translation.clj
@@ -2,20 +2,13 @@
 
 (defn translate-codon [codon]
      (case codon 
-       "AUG" "Methionine"
-       "UUU" "Phenylalanine"
-       "UUC" "Phenylalanine"
-       "UUA" "Leucine"
-       "UUG" "Leucine"
-       "UCU" "Serine"
-       "UCC" "Serine"
-       "UCA" "Serine"
-       "UCG" "Serine"
-       "UAU" "Tyrosine"
-       "UAC" "Tyrosine"
-       "UGU" "Cysteine"
-       "UGC" "Cysteine"
-       "UGG" "Tryptophan"
+       ("AUG") "Methionine"
+       ("UUU" "UUC") "Phenylalanine"
+       ("UUA" "UUG") "Leucine"
+       ("UCU" "UCC" "UCA" "UCG") "Serine"
+       ("UAU" "UAC") "Tyrosine"
+       ("UGU" "UGC") "Cysteine"
+       ("UGG") "Tryptophan"
        "STOP"))
 
 (defn translate-rna [rna]
