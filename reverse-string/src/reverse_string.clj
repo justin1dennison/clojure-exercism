651661943fb72babd5ad4ae0eac4875eb54e7cbe
Justin Dennison <justin1dennison@gmail.com>
2019-07-09 18:54:10 -0400

    simplified reverse-string by using reduce instead of a loop

--- reverse-string/src/reverse_string.clj
+++ reverse-string/src/reverse_string.clj
@@ -1,10 +1,4 @@
 (ns reverse-string)
 
 (defn reverse-string [s]
-  (loop [string s results []]
-    (if (zero? (count string))
-      (clojure.string/join "" results)
-      (recur 
-        (subs string 1) 
-        (cons (subs string 0 1) results)))))
-
+  (reduce #(str %2 %1) "" s)) 
