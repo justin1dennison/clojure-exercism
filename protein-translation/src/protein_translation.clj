9fad3eb0c1c329e0f08e22f7b6893a01402aced0
Justin Dennison <justin1dennison@gmail.com>
2019-07-09 12:50:20 -0400

    utilized threading macro to make protein translation easier

--- protein-translation/src/protein_translation.clj
+++ protein-translation/src/protein_translation.clj
@@ -1,18 +1,20 @@
 (ns protein-translation)
 
 (defn translate-codon [codon]
-     (case codon 
-       ("AUG") "Methionine"
-       ("UUU" "UUC") "Phenylalanine"
-       ("UUA" "UUG") "Leucine"
-       ("UCU" "UCC" "UCA" "UCG") "Serine"
-       ("UAU" "UAC") "Tyrosine"
-       ("UGU" "UGC") "Cysteine"
-       ("UGG") "Tryptophan"
-       "STOP"))
+  (case codon
+    ("AUG") "Methionine"
+    ("UUU" "UUC") "Phenylalanine"
+    ("UUA" "UUG") "Leucine"
+    ("UCU" "UCC" "UCA" "UCG") "Serine"
+    ("UAU" "UAC") "Tyrosine"
+    ("UGU" "UGC") "Cysteine"
+    ("UGG") "Tryptophan"
+    "STOP"))
 
 (defn translate-rna [rna]
-  (let [chunks (map #(clojure.string/join "" %) (partition 3 3 rna))]
-    (into [] (take-while 
-               #(not= % "STOP") 
-               (map translate-codon chunks)))))
+  (->> rna
+       (partition 3 3)
+       (map #(clojure.string/join "" %))
+       (map translate-codon)
+       (take-while #(not= % "STOP"))
+       (into [])))
