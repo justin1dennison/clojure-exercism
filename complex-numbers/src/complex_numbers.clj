1ab1336df7a29234502b4926fbd4aba7ac429834
Justin Dennison <justin1dennison@gmail.com>
2019-07-24 11:10:32 -0400

    tried a different solution for complex numbers

--- complex-numbers/src/complex_numbers.clj
+++ complex-numbers/src/complex_numbers.clj
@@ -1,29 +1,36 @@
 (ns complex-numbers)
 
+(defn- sum-of-squares [[a b]]
+  (+ (* a a) (* b b)))
+
 (defn real [[a b]] a)
 
 (defn imaginary [[a b]] b)
 
 (defn abs [[a b]] 
-  (Math/sqrt
-    (+ (* a a) (* b b))))
+  (->> [a b]
+       sum-of-squares
+       Math/sqrt))
 
 (defn conjugate [[a b]] [a (- b)])
 
-(defn add [[a b] [c d]] [(+ a c) (+ b d)])
+;; inspired from other solutions
+(def add (partial map +))
 
-(defn sub [[a b] [c d]] [(- a c) (- b d)])
+(def sub (partial map -))
 
+(defn process [[a b] [c d]]
+  [[(* a c) (* b d)]
+   [(* b c) (* a d)]])
+  
 (defn mul [[a b] [c d]] 
- (let [real (- (* a c) (* b d))
-       imag (+ (* b c) (* a d))]
-   [real imag]))
+ (let [[real imag] (process [a b] [c d])]
+   [(apply - real) (apply + imag)]))
 
 (defn div [[a b] [c d]] 
-  (let [real-top (float (+ (* a c) (* b d)))
-        imag-top (float (- (* b c) (* a d)))
-        bottom (float (+ (* c c) (* d d)))]
-    [
-     (/ real-top bottom)
-     (/ imag-top bottom)
-    ]))
+  (let [[real imag] (process [a b] [c d])
+        real-top (apply + real )
+        imag-top (apply - imag)
+        bottom (sum-of-squares [c d])]
+    (when-not (= 0 c d)
+      (map double [(/ real-top bottom) (/ imag-top bottom)]))))
