f6391cd50b919fe46a4008a90284d5a17d3436a4
Justin Dennison <justin1dennison@gmail.com>
2019-07-24 08:22:12 -0400

    simplified sublist

--- sublist/src/sublist.clj
+++ sublist/src/sublist.clj
@@ -1,35 +1,16 @@
 (ns sublist)
 
-(defn- equal-length? [xs ys]
-  "Tests length equality"
-  (= (count xs) (count ys)))
-
-(defn- equal? [xs ys]
-  "Determines whether the list x is equal to list y"
-  (and (equal-length? xs ys)
-       (= xs ys)))
-
-(defn- chunk [n lst]
-  (let [chunker (fn [idx _] (take n (drop idx lst)))]
-    (->> lst
-         (map-indexed chunker)
-         (filter #(= (count %) n)))))
-
 (defn superlist? [xs ys]
-  (let [xlen (count xs)
-        ylen (count ys)]
-    (and (> xlen ylen)
-         (->> xs
-              (chunk ylen)
-              (some #(equal? % ys))))))
-           
+   (->> xs
+        (partition (count ys) 1)
+        (some #(= % ys))))
 
 (defn sublist? [xs ys]
   (superlist? ys xs))
 
 (defn classify [fst snd]
   (cond 
-    (equal? fst snd) :equal
+    (= fst snd) :equal
     (superlist? fst snd) :superlist
     (sublist? fst snd) :sublist
     :else :unequal))
