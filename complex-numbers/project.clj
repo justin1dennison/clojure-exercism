25b5cf5e3942f661eed960e3d3bf03025922b3a5
Justin Dennison <justin1dennison@gmail.com>
2019-07-24 09:34:20 -0400

    added clojure formatter

--- complex-numbers/project.clj
+++ complex-numbers/project.clj
@@ -1,4 +1,5 @@
 (defproject complex-numbers "0.1.0-SNAPSHOT"
   :description "complex-numbers exercise"
   :url "https://github.com/exercism/clojure/tree/master/exercises/complex-numbers"
+  :plugins [[lein-cljfmt "0.6.4"]]
   :dependencies [[org.clojure/clojure "1.10.0"]])
