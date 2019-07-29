8444843df9c1ee1f957adb39f52bed5c7212696e
Justin Dennison <justin1dennison@gmail.com>
2019-07-29 07:42:45 -0400

    removed extraneous commas

--- scrabble-score/src/scrabble_score.clj
+++ scrabble-score/src/scrabble_score.clj
@@ -5,13 +5,13 @@
 (defn score-letter [letter] 
   (let [letter (s/upper-case (str letter))]
     (case letter
-      ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T") 1
-      ("D", "G") 2
-      ("B", "C", "M", "P") 3
-      ("F", "H", "V", "W", "Y") 4
+      ("A" "E" "I" "O" "U" "L" "N" "R" "S" "T") 1
+      ("D" "G") 2
+      ("B" "C" "M" "P") 3
+      ("F" "H" "V" "W" "Y") 4
       ("K") 5
-      ("J", "X") 8
-      ("Q", "Z") 10)))
+      ("J" "X") 8
+      ("Q" "Z") 10)))
 
 (defn score-word [word] 
   (reduce + 0 
