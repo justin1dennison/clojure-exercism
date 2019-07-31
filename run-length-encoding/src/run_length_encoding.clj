3a82532edf404fd1af560035d7e43ca1a22d5032
Justin Dennison <justin1dennison@gmail.com>
2019-07-31 10:23:13 -0400

    brute force solution to run-length-encoding

---
+++ run-length-encoding/src/run_length_encoding.clj
@@ -0,0 +1,38 @@
+(ns run-length-encoding
+  (:require [clojure.string :refer [join]]))
+
+(defn one? [n]
+  (= n 1))
+
+(defn run-length-encode
+  "encodes a string with run-length-encoding"
+  [plain-text]
+  (if (empty? plain-text)
+    plain-text
+    (let [parts (partition-by identity plain-text)
+          counts (into [] cat (map frequencies parts))]
+      (join
+       (map
+        (fn [[letter n]]
+          (str (if  (one? n) "" n) letter))
+        counts)))))
+
+(def sample "12WB12W3B24WB")
+(defn digit? [n] (Character/isDigit n))
+(defn letter? [n] (Character/isLetter n))
+
+(defn run-length-decode
+  "decodes a run-length-encoded string"
+  [cipher-text]
+  (loop [start cipher-text acc []]
+    (if (empty? start)
+      (join (map (partial apply str) acc))
+      (let [n (do (take-while digit? start))
+            digit-count (count n)
+            c (take 1 (drop digit-count start))
+            letter-count (count c)
+            nxt (do (drop (+ digit-count letter-count) start))
+            num (Integer/parseInt (if (empty? n) "1" (apply str n)))
+            letter (apply str c)]
+        (recur nxt
+               (conj acc (repeat num letter)))))))
