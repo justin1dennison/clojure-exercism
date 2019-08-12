e4afbcb072d6d5ee814f681d0685d31337304bfd
Justin Dennison <justin1dennison@gmail.com>
2019-08-12 14:30:20 -0400

    refactored decoding in run length encoding

--- run-length-encoding/src/run_length_encoding.clj
+++ run-length-encoding/src/run_length_encoding.clj
@@ -5,6 +5,7 @@
   (= n 1))
 (defn digit? [n] (Character/isDigit n))
 (defn letter? [n] (Character/isLetter n))
+
 (defn encode-part [[letter n]]
   (str (if  (one? n) "" n) letter))
 
@@ -18,18 +19,21 @@
        join))
 
 
+(defn decode-part [g]
+  (let [letter (last g)
+        number (join "" (drop-last g))]
+    (if (empty? number)
+      (str letter)
+      (join
+       (repeat (Integer/parseInt number) letter)))))
+
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
-  (loop [start cipher-text acc []]
-    (if (empty? start)
-      (join (map (partial apply str) acc))
-      (let [n (take-while digit? start)
-            digit-count (count n)
-            c (take 1 (drop digit-count start))
-            letter-count (count c)
-            nxt (drop (+ digit-count letter-count) start)
-            num (Integer/parseInt (if (empty? n) "1" (apply str n)))
-            letter (apply str c)]
-        (recur nxt
-               (conj acc (repeat num letter)))))))
+  (->> cipher-text
+       (re-seq #"[0-9]{0,2}[a-zA-Z ]")
+       (map decode-part)
+       (join "")))
+
+
+
