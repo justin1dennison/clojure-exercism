4f6ee2637512a41f90484131835a8ab103b65be7
Justin Dennison <justin1dennison@gmail.com>
2019-08-12 15:23:38 -0400

    cleaned up

--- run-length-encoding/src/run_length_encoding.clj
+++ run-length-encoding/src/run_length_encoding.clj
@@ -3,14 +3,11 @@
 
 (defn- one? [n]
   (= n 1))
-(defn- digit? [n] (Character/isDigit n))
-(defn- letter? [n] (Character/isLetter n))
 
 (defn- encode-part [part]
   (let [n (count part)
         letter (first part)]
-    (str (if (one? n) "" n) letter)))
-
+    (str (when-not (one? n) n) letter)))
 
 (defn run-length-encode
   "encodes a string with run-length-encoding"
@@ -19,21 +16,17 @@
        (map encode-part)
        join))
 
-(defn- decode-part [g]
-  (let [letter (last g)
-        number (join "" (drop-last g))]
-    (if (empty? number)
-      (str letter)
-      (join
-       (repeat (Integer/parseInt number) letter)))))
+(defn- decode-part [[_ number letter]]
+  (if (empty? number)
+    letter
+    (join
+     (repeat (Integer/parseInt number) letter))))
 
 (defn run-length-decode
   "decodes a run-length-encoded string"
   [cipher-text]
   (->> cipher-text
-       (re-seq #"[0-9]{0,2}[a-zA-Z ]")
+       (re-seq #"([0-9]+)?([a-zA-Z ])")
        (map decode-part)
-       (join "")))
-
-
+       join))
 
