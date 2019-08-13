1f0ed6eb855eab25e28979f4c4c0075fd6ed8bde
Justin Dennison <justin1dennison@gmail.com>
2019-08-13 10:07:54 -0400

    defined discard with retain

--- strain/src/strain.clj
+++ strain/src/strain.clj
@@ -3,7 +3,7 @@
 (defn retain [pred coll]
   (for [x coll :when (pred x)] x))
 
-(defn discard [pred coll]
-  (for [x coll :when ((complement pred) x)] x))
+(defn discard [pred coll] (retain (complement pred) coll))
+
 
 
