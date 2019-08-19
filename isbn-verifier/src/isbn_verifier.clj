7bd906a0660b2b36cf0ea20bf83c9b6310d11c56
Justin Dennison <justin1dennison@gmail.com>
2019-08-19 16:01:19 -0400

    combine map and reduce into a single reducer

--- isbn-verifier/src/isbn_verifier.clj
+++ isbn-verifier/src/isbn_verifier.clj
@@ -1,7 +1,6 @@
 (ns isbn-verifier
   (:require [clojure.string :as s]))
 
-
 (def re-isbn-pattern #"[0-9]{9}[0-9X]{1}")
 
 (defn- char->int [c]
@@ -9,27 +8,23 @@
     10
     (- (int c) 48)))
 
-
-(defn- part-calculation [idx element]
-  (* element (inc idx)))
-
 (defn- mod-eleven [n]
   (mod n 11))
 
 (defn isbn? [isbn]
   (let [cleaned (s/replace isbn #"-" "")
-        match (re-find re-isbn-pattern cleaned)]
+        match (re-find re-isbn-pattern cleaned)
+        calculation #(* %1 (inc %2))]
     (if (or (> (count cleaned) 10) (nil? match))
       false
-      (zero? (->> match
-                  (map char->int)
-                  (reverse)
-                  (map-indexed part-calculation)
-                  (reduce +)
-                  mod-eleven)))))
-
-(isbn? "11111111111")
-
-
+      (->> match
+           (map char->int)
+           (reverse)
+           (reduce (fn [[sum idx] element]
+                     [(+ sum (calculation element idx))
+                      (inc idx)]) [0 0])
+           first
+           mod-eleven
+           zero?))))
 
 
