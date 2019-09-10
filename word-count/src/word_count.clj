791bafec473c3577df72fdf04e6c60633a679da4
Justin Dennison <justin1dennison@gmail.com>
2019-09-10 14:43:17 -0400

    simplified word count by using a different regular expression

--- word-count/src/word_count.clj
+++ word-count/src/word_count.clj
@@ -1,25 +1,10 @@
 (ns word-count
   (:require [clojure.string :as str]))
 
-(def ^:private punctuation ",!?@#$%&^*():")
-
-(defn- strip [chars coll]
-  (apply str (remove (set chars) coll)))
-
-(def ^:private remove-punctuation (partial strip punctuation))
-
-(defn- flip-split [pattern s]
-  (str/split s pattern))
-
-(defn- normalize-spacing [s]
-  (str/replace s #"\s{2,}" " "))
-
 (defn word-count [s]
   (->> s
        str/lower-case
-       remove-punctuation
-       normalize-spacing
-       (flip-split #" ")
+       (re-seq #"\w+")
        frequencies))
 
 
