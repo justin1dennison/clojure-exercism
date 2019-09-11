a103e08dfb49e44834a6461146c60e9c67e58a70
Justin Dennison <justin1dennison@gmail.com>
2019-09-11 07:56:54 -0400

    finished anagram

---
+++ anagram/src/anagram.clj
@@ -0,0 +1,17 @@
+(ns anagram
+  (:require [clojure.set :as set]
+            [clojure.string :as s]))
+
+(defn anagram? [target source]
+  (let [target (s/lower-case target)
+        source (s/lower-case source)]
+    (and
+     (not= target source)
+     (= (frequencies target) (frequencies source))
+     (= (count source) (count target))
+     (set/subset? (set source) (set target)))))
+
+(defn anagrams-for [word prospect-list]
+  (->> prospect-list
+       (filter (partial anagram? (s/lower-case word)))))
+
