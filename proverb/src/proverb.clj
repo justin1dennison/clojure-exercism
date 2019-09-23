5ee1fd9fcb8a0bef72d10dbed76808ec42348501
Justin Dennison <justin1dennison@gmail.com>
2019-09-23 14:06:39 -0400

    some clean up on the proverb solution

--- proverb/src/proverb.clj
+++ proverb/src/proverb.clj
@@ -1,16 +1,22 @@
 (ns proverb
   (:require [clojure.string :as str]))
 
+(defn line [[l r]]
+  (format "For want of a %s the %s was lost." l r))
+
+(defn end [word]
+  (format "And all for the want of a %s." word))
+
 (defn recite [words]
-  (let [fst             (first words)
-        chunks          (partition 2 1 words)
-        beginning-parts (for [[l r] chunks] (format "For want of a %s the %s was lost." l r))
-        ending          (format "And all for the want of a %s." fst)]
+  (let [fst       (first words)
+        chunks    (partition 2 1 words)
+        beginning (map line chunks)
+        ending    (end fst)]
     (if (empty? words)
       ""
       (str/join
        "\n"
-       (conj (into [] beginning-parts) ending)))))
+       (conj (into [] beginning) ending)))))
 
 
 
