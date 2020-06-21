6038af6917c7cf46ad4941babf028efaaf29ba73
Justin Dennison <justin1dennison@gmail.com>
2020-06-20 23:05:36 -0400

    completed say

---
+++ say/project.clj
@@ -0,0 +1,5 @@
+(defproject say "0.1.0-SNAPSHOT"
+  :description "say exercise."
+  :url "https://github.com/exercism/clojure/tree/master/exercises/say"
+  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :plugins [[lein-cljfmt "0.6.7"]])
---
+++ say/src/say.clj
@@ -0,0 +1,53 @@
+(ns say)
+
+(def small-numbers ["zero", "one", "two", "three", "four", "five", "six",
+                    "seven", "eight", "nine", "ten", "eleven", "twelve",
+                    "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                    "eighteen", "nineteen"])
+
+(def tens ["twenty", "thirty", "forty", "fifty", "sixty",
+           "seventy", "eighty", "ninety"])
+
+(def thousands 1000)
+(def millions 1000000)
+(def billions 1000000000)
+(def trillions 1000000000000)
+
+(defn number [num]
+  (cond
+    (< num 0) (throw (IllegalArgumentException. "Cannot be less than zero"))
+    (> num (dec trillions)) (throw (IllegalArgumentException. "Too Big"))
+    (< num 20) (get small-numbers num)
+    (< num 100) (let [base (get tens (- (quot num 10) 2))
+                      remainder (mod num 10)
+                      ones  (get small-numbers remainder)]
+                  (if (zero? remainder) base (str base "-" ones)))
+    (< num thousands) (let [d (quot num 100)
+                            remainder (mod num 100)
+                            base (get small-numbers d)
+                            unit "hundred"]
+                        (if (zero? remainder)
+                          (str base " " unit)
+                          (str (number d) " " unit " " (number remainder))))
+    (< num millions) (let [d (quot num 1000)
+                           remainder (mod num 1000)
+                           base (get small-numbers d)
+                           unit "thousand"]
+                       (if (zero? remainder)
+                         (str base " " unit)
+                         (str (number d) " " unit " " (number remainder))))
+    (< num billions) (let [d (quot num 1000000)
+                           remainder (mod num 1000000)
+                           base (get small-numbers d)
+                           unit "million"]
+                       (if (zero? remainder)
+                         (str base " " unit)
+                         (str (number d) " " unit " " (number remainder))))
+    :else (let [d (quot num 1000000000)
+                remainder (mod num 1000000000)
+                base (get small-numbers d)
+                unit "billion"]
+            (if (zero? remainder)
+              (str base " " unit)
+              (str (number d) " " unit " " (number remainder))))))
+
